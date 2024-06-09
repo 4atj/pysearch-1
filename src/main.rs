c039289980b2f5f7fea6ee2b20f61175ac6e3bb7
4atj <fourajbira16@gmail.com>
2024-06-09 21:23:42 +0100

    Support Multiple Expressions

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ pub mod params;
 #[cfg_attr(not(feature = "simd"), path = "vec.rs")]
 pub mod vec;
 
-use expr::{Expr, Mask, NonNullExpr};
+use expr::{Expr, NonNullExpr, VarCount};
 use operator::*;
 use params::*;
 
@@ -35,28 +35,73 @@ fn positive_integer_length(mut k: Num) -> usize {
     l
 }
 
-fn can_use_required_vars(mask: u8, length: usize) -> bool {
-    !USE_ALL_VARS || length + (INPUTS.len() - (mask.count_ones() as usize)) * 2 <= MAX_LENGTH
+fn can_use_required_vars(var_count: VarCount, length: usize) -> bool {
+    let missing_uses: u8 = var_count
+        .iter()
+        .zip(INPUTS.iter())
+        .map(|(&c, i)| i.min_uses - std::cmp::min(c, i.min_uses))
+        .sum();
+    length + missing_uses as usize * 2 <= MAX_LENGTH
+}
+
+fn uses_required_vars(var_count: VarCount) -> bool {
+    var_count
+        .iter()
+        .zip(INPUTS.iter())
+        .all(|(&c, i)| c >= i.min_uses)
+}
+
+fn sum_var_counts(mut var_count: VarCount, var_count_: VarCount) -> Option<VarCount> {
+    for ((l, &r), i) in var_count
+        .iter_mut()
+        .zip(var_count_.iter())
+        .zip(INPUTS.iter())
+    {
+        *l += r;
+        if *l > i.max_uses {
+            return None;
+        }
+    }
+    Some(var_count)
 }
 
 fn is_leaf_expr(op_idx: OpIndex, length: usize) -> bool {
-    length == MAX_LENGTH
-        || length == MAX_LENGTH - 1 && (UNARY_OPERATORS.len() == 0 || op_idx.prec() < UnaryOp::PREC)
+    length == MAX_EXPR_LENGTH
+        || length == MAX_EXPR_LENGTH - 1
+            && (UNARY_OPERATORS.len() == 0 || op_idx.prec() < UnaryOp::PREC)
+}
+
+const fn has_unlimited_var() -> bool {
+    let mut i = 0;
+    while i < INPUTS.len() {
+        if INPUTS[i].max_uses == u8::MAX {
+            return true;
+        }
+        i += 1;
+    }
+    false
 }
 
 fn save(level: &mut CacheLevel, expr: Expr, n: usize, cache: &Cache, hashset_cache: &HashSetCache) {
-    const ALL_MASK: Mask = (1 << INPUTS.len()) - 1;
+    if NUM_EXPRS == 1 {
+        if uses_required_vars(expr.var_count) && Matcher::match_all(&expr) {
+            println!("{expr}");
+            return;
+        }
 
-    if (!USE_ALL_VARS || expr.var_mask == ALL_MASK) && Matcher::match_all(&expr) {
-        println!("{expr}");
-        return;
+        if !MATCH_1BY1 && is_leaf_expr(expr.op_idx, n) {
+            return;
+        }
     }
 
-    if !MATCH_1BY1 && is_leaf_expr(expr.op_idx, n) {
-        return;
-    }
+    let cant_use_more_vars = !has_unlimited_var()
+        && expr
+            .var_count
+            .iter()
+            .zip(INPUTS.iter())
+            .all(|(&c, i)| c == i.max_uses);
 
-    if !REUSE_VARS && expr.var_mask == ALL_MASK {
+    if cant_use_more_vars {
         let mut mp: HashMap<Num, Num> = HashMap::new();
         for i in 0..GOAL.len() {
             if let Some(old) = mp.insert(expr.output[i], GOAL[i]) {
@@ -77,11 +122,25 @@ fn save(level: &mut CacheLevel, expr: Expr, n: usize, cache: &Cache, hashset_cac
     }
 
     if n > MAX_CACHE_LENGTH {
-        for dfs_len in n + 2..=MAX_LENGTH {
+        if NUM_EXPRS > 1 {
+            for dfs_len in n + (NUM_EXPRS - 1) * (MIN_EXPR_LENGTH + 1)..=MAX_LENGTH {
+                find_multi_exprs_dfs(
+                    cache,
+                    [&expr; NUM_EXPRS],
+                    expr.var_count,
+                    &expr,
+                    n,
+                    0,
+                    dfs_len,
+                    0,
+                );
+            }
+        }
+        for dfs_len in n + 2..=MAX_EXPR_LENGTH {
             find_binary_expressions_left(level, cache, hashset_cache, dfs_len, n, &expr);
             find_binary_expressions_right(level, cache, hashset_cache, dfs_len, n, &expr);
         }
-        if n + 1 <= MAX_LENGTH {
+        if n + 1 <= MAX_EXPR_LENGTH {
             find_unary_operators(level, cache, hashset_cache, n + 1, &expr);
         }
         if !is_leaf_expr(OP_INDEX_PARENS, n + 2) && expr.op_idx < OP_INDEX_PARENS {
@@ -99,6 +158,88 @@ fn save(level: &mut CacheLevel, expr: Expr, n: usize, cache: &Cache, hashset_cac
     level.push(expr);
 }
 
+fn find_multi_exprs_dfs<'a>(
+    cache: &'a Cache,
+    mut exprs: [&'a Expr; NUM_EXPRS],
+    var_count: VarCount,
+    expr: &'a Expr,
+    expr_length: usize,
+    index: usize,
+    n: usize,
+    k: usize,
+) {
+    find_multi_exprs(cache, exprs, var_count, index + 1, n, k + expr_length + 1);
+
+    let is_last_expr = index + 1 == NUM_EXPRS;
+    if is_last_expr {
+        return;
+    }
+
+    let max_expr_len = n + 1 - k - (NUM_EXPRS - index - 2) * (MIN_EXPR_LENGTH + 1) - expr_length;
+    let min_expr_len = MIN_EXPR_LENGTH;
+
+    for new_expr_len in min_expr_len..=max_expr_len.min(cache.len() - 1) {
+        for new_expr in cache[new_expr_len].iter() {
+            exprs[index] = new_expr;
+            if let Some(new_var_count) = sum_var_counts(var_count, new_expr.var_count) {
+                find_multi_exprs_dfs(
+                    cache,
+                    exprs,
+                    new_var_count,
+                    expr,
+                    expr_length,
+                    index + 1,
+                    n,
+                    k + new_expr_len + 1,
+                );
+            }
+        }
+    }
+}
+
+fn find_multi_exprs<'a>(
+    cache: &'a Cache,
+    mut exprs: [&'a Expr; NUM_EXPRS],
+    var_count: VarCount,
+    index: usize,
+    n: usize,
+    k: usize,
+) {
+    if index == NUM_EXPRS {
+        if uses_required_vars(var_count) && Matcher::match_multi_exprs_all(exprs) {
+            for (index, expr) in exprs.iter().enumerate() {
+                match index + 1 {
+                    NUM_EXPRS => println!("{expr}"),
+                    _ => print!("{expr},"),
+                }
+            }
+        }
+        return;
+    }
+
+    let is_last_expr = index + 1 == NUM_EXPRS;
+    let max_expr_len = n - k - (NUM_EXPRS - index - 1) * (MIN_EXPR_LENGTH + 1);
+    let min_expr_len = if is_last_expr {
+        max_expr_len
+    } else {
+        MIN_EXPR_LENGTH
+    };
+
+    for new_expr_len in min_expr_len..=max_expr_len.min(cache.len() - 1) {
+        for new_expr in cache[new_expr_len].iter() {
+            exprs[index] = new_expr;
+            find_multi_exprs(
+                cache,
+                exprs,
+                new_expr.var_count,
+                index + 1,
+                n,
+                k + new_expr_len + 1,
+            );
+        }
+    }
+}
+
 #[inline(always)]
 fn find_binary_operators(
     cn: &mut CacheLevel,
@@ -112,17 +253,17 @@ fn find_binary_operators(
     if er.is_literal() && el.is_literal() {
         return;
     }
-    if !REUSE_VARS && (el.var_mask & er.var_mask != 0) {
-        return;
-    }
-    let mask = el.var_mask | er.var_mask;
-    if !can_use_required_vars(mask, n) {
+    let var_count = match sum_var_counts(el.var_count, er.var_count) {
+        Some(var_count) => var_count,
+        None => return,
+    };
+    if !can_use_required_vars(var_count, n) {
         return;
     }
     seq!(idx in 0..100 {
         if let (Some(&op_idx), Some(op)) = (OP_BINARY_INDEX_TABLE.get(idx), BINARY_OPERATORS.get(idx)) {
             if op.name.len() == op_len && op.can_apply(el, er) {
-                if MATCH_1BY1 && is_leaf_expr(op_idx, n) {
+                if MATCH_1BY1 && NUM_EXPRS == 1 && is_leaf_expr(op_idx, n) {
                     let mut matcher = Matcher::new();
                     if el
                         .output
@@ -140,7 +281,7 @@ fn find_binary_operators(
                 } else if let Some(output) = op.vec_apply(el.output.clone(), &er.output) {
                     save(
                         cn,
-                        Expr::bin(el.into(), er.into(), op_idx, mask, output),
+                        Expr::bin(el.into(), er.into(), op_idx, var_count, output),
                         n,
                         cache,
                         hashset_cache,
@@ -194,13 +335,13 @@ fn find_unary_operators(
     n: usize,
     er: &Expr,
 ) {
-    if !can_use_required_vars(er.var_mask, n) {
+    if !can_use_required_vars(er.var_count, n) {
         return;
     }
     seq!(idx in 0..10 {
         if let (Some(&op_idx), Some(op)) = (OP_UNARY_INDEX_TABLE.get(idx), UNARY_OPERATORS.get(idx)) {
             if op.can_apply(er) {
-                if MATCH_1BY1 && is_leaf_expr(op_idx, n) {
+                if MATCH_1BY1 && NUM_EXPRS == 1 && is_leaf_expr(op_idx, n) {
                     let mut matcher = Matcher::new();
                     if er
                         .output
@@ -249,7 +390,7 @@ fn find_parens_expressions(
         return;
     }
     for er in &cache[n - 2] {
-        if !can_use_required_vars(er.var_mask, n) {
+        if !can_use_required_vars(er.var_count, n) {
             continue;
         }
         if er.op_idx < OP_INDEX_PARENS {
@@ -323,6 +464,23 @@ fn find_expressions_multithread(
     let cache = &mut_cache;
     let hashset_cache = &mut_hashset_cache;
 
+    if NUM_EXPRS > 1 {
+        let max_expr_len = n - (NUM_EXPRS - 1) * (MIN_EXPR_LENGTH + 1);
+        (MIN_EXPR_LENGTH..=std::cmp::min(cache.len() - 1, max_expr_len))
+            .into_par_iter()
+            .flat_map(|k| {
+                cache[k].par_iter().map(move |expr| {
+                    find_multi_exprs(cache, [expr; NUM_EXPRS], expr.var_count, 1, n, k + 1);
+                })
+            })
+            .for_each(|_| {});
+    }
+
+    if n > MAX_EXPR_LENGTH {
+        add_to_cache(CacheLevel::new(), mut_cache, mut_hashset_cache);
+        return;
+    }
+
     let mut cn = (1..n - 1)
         .into_par_iter()
         .flat_map(|k| {
@@ -357,26 +515,48 @@ fn find_expressions_multithread(
 }
 
 fn find_expressions(cache: &mut Cache, hashset_cache: &mut HashSetCache, n: usize) {
+    if NUM_EXPRS > 1 {
+        let max_expr_len = n - (NUM_EXPRS - 1) * (MIN_EXPR_LENGTH + 1);
+        for k in MIN_EXPR_LENGTH..=std::cmp::min(cache.len() - 1, max_expr_len) {
+            for expr in &cache[k] {
+                find_multi_exprs(cache, [expr; NUM_EXPRS], expr.var_count, 1, n, k + 1);
+            }
+        }
+    }
     let mut cn = CacheLevel::new();
-    find_variables_and_literals(&mut cn, n);
-    find_parens_expressions(&mut cn, cache, hashset_cache, n);
-    find_unary_expressions(&mut cn, cache, hashset_cache, n);
-    for k in 1..n - 1 {
-        for r in &cache[k] {
-            find_binary_expressions_left(&mut cn, cache, hashset_cache, n, k, r);
+    if n <= MAX_EXPR_LENGTH {
+        find_variables_and_literals(&mut cn, n);
+        find_parens_expressions(&mut cn, cache, hashset_cache, n);
+        find_unary_expressions(&mut cn, cache, hashset_cache, n);
+        for k in 1..n - 1 {
+            for r in &cache[k] {
+                find_binary_expressions_left(&mut cn, cache, hashset_cache, n, k, r);
+            }
         }
     }
     add_to_cache(cn, cache, hashset_cache);
 }
 
 fn validate_input() {
+    assert_ne!(NUM_EXPRS, 0, "The number of expressions must be non-zero");
+    assert!(
+        NUM_EXPRS * (MIN_EXPR_LENGTH + 1) <= MAX_LENGTH + 1,
+        "The minimum expression length is not valid"
+    );
     for i in INPUTS {
         assert_eq!(
             i.vec.len(),
             GOAL.len(),
             "INPUTS and GOAL must have equal length"
         );
+
+        assert_ne!(i.max_uses, 0, "INPUTS maximum uses must be non-zero");
     }
+
+    assert!(
+        INPUTS.iter().map(|i| i.min_uses as usize).sum::<usize>() * 2 <= MAX_LENGTH + 1,
+        "The minimum uses requirement will never be met"
+    );
 
     let mut input_set = HashSet::new();
     for i in 0..INPUTS[0].vec.len() {
--- src/params.rs
+++ src/params.rs
@@ -5,11 +5,15 @@ pub type Num = i32;
 pub struct Input {
     pub name: &'static str,
     pub vec: &'static [Num],
+    pub min_uses: u8,
+    pub max_uses: u8,
 }
 
 pub const INPUTS: &[Input] = &[Input {
     name: "n",
     vec: &['E' as i32, 'W' as i32, 'N' as i32, 'S' as i32],
+    min_uses: 1,
+    max_uses: 255,
 }];
 
 pub struct Matcher {}
@@ -40,6 +44,15 @@ impl Matcher {
                 expr.op_idx,
             )
     }
+
+    fn match_multi_exprs_one(&mut self, _index: usize, _exprs: [&Expr; NUM_EXPRS]) -> bool {
+        true
+    }
+
+    pub fn match_multi_exprs_all(exprs: [&Expr; NUM_EXPRS]) -> bool {
+        let mut matcher = Self::new();
+        (0..exprs[0].output.len()).all(|i| matcher.match_multi_exprs_one(i, exprs))
+    }
 }
 
 pub const GOAL: &[Num] = &[1, -1, 0, 0];
@@ -47,12 +60,14 @@ pub const GOAL: &[Num] = &[1, -1, 0, 0];
 pub const MAX_LENGTH: usize = 14;
 pub const MAX_CACHE_LENGTH: usize = 10;
 pub const MIN_MULTITHREAD_LENGTH: usize = MAX_CACHE_LENGTH + 1;
-pub const LITERALS: &[Num] = &[
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-];
+
+pub const NUM_EXPRS: usize = 1;
+pub const MIN_EXPR_LENGTH: usize = 1;
+pub const MAX_EXPR_LENGTH: usize = MAX_LENGTH - (NUM_EXPRS - 1) * (MIN_EXPR_LENGTH + 1);
+
+pub const LITERALS: &[Num] = &[];
 /// If not 0, include all numbers in 1..=MAX_LITERAL in addition to LITERALS.
-pub const MAX_LITERAL: Num = 0;
+pub const MAX_LITERAL: Num = 40;
 
 #[rustfmt::skip]
 pub const BINARY_OPERATORS: &[BinaryOp] = &[
@@ -101,9 +116,3 @@ pub const UNARY_OPERATORS: &[UnaryOp] = &[
 
 /// Match leaf expressions 1 output at a time to avoid unnecessary precalculations
 pub const MATCH_1BY1: bool = true;
-
-/// Search expressions that use the same variable twice (like `x*x`).
-pub const REUSE_VARS: bool = true;
-
-/// Controls whether all declared variables should be always used.
-pub const USE_ALL_VARS: bool = true;
